c27ddfff3b6521d3cfeb5c1a16b5ecc11defb48c
Stiflerzak <hilarymwendia97@gmail.com>
2022-12-06 14:20:54 +0300

    Partially complete

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution in this file!
+const drivers= ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+function  returnFirstTwoDrivers() {
+ return drivers.slice(0,2)
+
+}
+
+returnFirstTwoDrivers();
+
+function  returnLastTwoDrivers() {
+    return drivers.slice(2,4)
+   
+   }
+   
+   returnLastTwoDrivers()
+
+   function selectingDrivers(){
+
+   }
+   
